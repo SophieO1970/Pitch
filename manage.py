e629d0de98a563eedb1bdea2cda4145053886a43
sophieO1970 <sophieO1970@gmail.com>
2020-10-25 16:34:35 +0300

    add an app instance on manage.py

---
+++ manage.py
@@ -0,0 +1,31 @@
+from app import create_app, db
+from flask_script import Manager, Server
+from app.models import User
+from flask_migrate import Migrate, MigrateCommand
+
+# Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+manager.add_command('server', Server)
+
+
+@manager.command
+def test():
+    '''
+    Run the unit test
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Pitch=Pitch, Comment=Comment, UpVote=UpVote, DownVote=DownVote, PhotoProfile=PhotoProfile)
+
+
+if __name__ == '__main__':
+    manager.run()
