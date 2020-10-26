0f5312f8cd5ffa7f4f56df535dc0f7b0a28f59a9
sophieO1970 <sophieO1970@gmail.com>
2020-10-26 16:44:40 +0300

    edit the forms.py and views.py files

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -13,13 +13,14 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self,data_field):
         if User.query.filter_by(email =data_field.data).first():
-            raise ValidationError('There is an account with that email')
+            raise ValidationError('Sorry! There is an account with that email')
 
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('That username is taken')
+            raise ValidationError('Ooops!That username is taken')
 
 class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[Required()])
     email = StringField('Your Email Address',validators=[Required(),Email()])
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
